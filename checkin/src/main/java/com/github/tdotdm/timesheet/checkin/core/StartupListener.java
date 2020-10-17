9c7a7b087628b5bf9bb4c7d35e30abbb4a79e760
Darryn McGaw <dm.maxton@gmail.com>
2020-10-17 17:12:30 +0100

    Create core package from StartupListener

---
+++ checkin/src/main/java/com/github/tdotdm/timesheet/checkin/core/StartupListener.java
@@ -0,0 +1,12 @@
+package com.github.tdotdm.timesheet.checkin.core;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupListener implements CommandLineRunner {
+    @Override
+    public void run(final String... args) {
+
+    }
+}
