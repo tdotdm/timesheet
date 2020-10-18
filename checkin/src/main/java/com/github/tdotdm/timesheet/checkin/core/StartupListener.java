f43c8ab5bc38c92fc01c87c786312222ee10e8eb
Darryn McGaw <dm.maxton@gmail.com>
2020-10-18 17:33:45 +0100

    Add TimesheetService to StartupListener

--- checkin/src/main/java/com/github/tdotdm/timesheet/checkin/core/StartupListener.java
+++ checkin/src/main/java/com/github/tdotdm/timesheet/checkin/core/StartupListener.java
@@ -1,12 +1,45 @@
 package com.github.tdotdm.timesheet.checkin.core;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class StartupListener implements CommandLineRunner {
+    private final TimesheetService timesheetService;
+
     @Override
     public void run(final String... args) {
+        final boolean workingDirectoryIsReady = workingDirectoryIsReady();
+        if (workingDirectoryIsReady) {
+            final Timesheet timesheet = timesheetService.read();
+            timesheet.addRecord(Record.newInRecord());
+            timesheetService.create(timesheet);
+        } else {
+            log.error("Cannot update timesheet; application stopping.");
+        }
+    }
 
+    private boolean workingDirectoryIsReady() {
+        try {
+            log.info("Searching for timesheet.");
+            final File file = new File("bin/timesheet.json");
+            if (!file.exists()) {
+                log.error("Timesheet not found; creating a new one.");
+                return file.createNewFile();
+            }
+
+            return true;
+        } catch (final IOException e) {
+            log.error("Cannot create timesheet.");
+        }
+
+        return false;
     }
 }
