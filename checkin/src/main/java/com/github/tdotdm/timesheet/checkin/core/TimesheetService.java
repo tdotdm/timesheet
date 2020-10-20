86ba9f506f63345285ca544603d875b83cd1dab5
Darryn McGaw <dm.maxton@gmail.com>
2020-10-20 22:14:32 +0100

    Mark TimesheetService as final

--- checkin/src/main/java/com/github/tdotdm/timesheet/checkin/core/TimesheetService.java
+++ checkin/src/main/java/com/github/tdotdm/timesheet/checkin/core/TimesheetService.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class TimesheetService {
+public final class TimesheetService {
     private static final String TIMESHEET_LOCATION = "bin/timesheet.json";
 
     private final Gson gson = new GsonBuilder().create();
