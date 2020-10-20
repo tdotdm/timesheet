a8623d1641661c65a38573ad340a5baa14989fee
Darryn McGaw <dm.maxton@gmail.com>
2020-10-20 22:02:14 +0100

    Finalise Timesheet impl

--- checkin/src/main/java/com/github/tdotdm/timesheet/checkin/core/Timesheet.java
+++ checkin/src/main/java/com/github/tdotdm/timesheet/checkin/core/Timesheet.java
@@ -1,9 +1,12 @@
 package com.github.tdotdm.timesheet.checkin.core;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Timesheet {
+    @Getter
     private final List<Record> records = new ArrayList<>();
 
     public void addRecord(final Record record) {
