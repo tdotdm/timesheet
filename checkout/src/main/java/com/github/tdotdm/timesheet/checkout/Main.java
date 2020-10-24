1e69e99a2d8be32fc63b738ef3a42a9e15fb2b79
Darryn McGaw <dm.maxton@gmail.com>
2020-10-24 20:01:55 +0100

    Add SuppressWarning annotation to Main for check in and check out

--- checkout/src/main/java/com/github/tdotdm/timesheet/checkout/Main.java
+++ checkout/src/main/java/com/github/tdotdm/timesheet/checkout/Main.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value = "com.github.tdotdm.timesheet")
+@SuppressWarnings("HideUtilityClassConstructor")
 public class Main {
     public static void main(final String[] args) {
         SpringApplication.run(Main.class, args);
