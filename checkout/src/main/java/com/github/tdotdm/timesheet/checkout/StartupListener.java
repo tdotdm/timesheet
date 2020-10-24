4e05646f29493536e6d614905495f19133bff953
Darryn McGaw <dm.maxton@gmail.com>
2020-10-24 22:24:20 +0100

    Remove Slf4j annotation from Checkout StartupListener

--- checkout/src/main/java/com/github/tdotdm/timesheet/checkout/StartupListener.java
+++ checkout/src/main/java/com/github/tdotdm/timesheet/checkout/StartupListener.java
@@ -3,11 +3,9 @@ package com.github.tdotdm.timesheet.checkout;
 import com.github.tdotdm.timesheet.library.Action;
 import com.github.tdotdm.timesheet.library.ServiceRoute;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class StartupListener implements CommandLineRunner {
