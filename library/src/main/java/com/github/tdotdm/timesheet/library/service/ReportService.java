36cdfd2c4b5e52adf6b22d7d49977287c7aba9ee
Darryn McGaw <dm.maxton@gmail.com>
2020-10-25 19:11:25 +0000

    Add ReportService

---
+++ library/src/main/java/com/github/tdotdm/timesheet/library/service/ReportService.java
@@ -0,0 +1,12 @@
+package com.github.tdotdm.timesheet.library.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ReportService {
+    public void report() {
+
+    }
+}
