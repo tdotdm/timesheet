c72a260bd7e90513cf95117d254f6d097729a4dc
Darryn McGaw <darryn.mcgaw@outlook.com>
2020-11-24 21:36:07 +0000

    Add StatusService to library

---
+++ library/src/main/java/com/github/tdotdm/timesheet/library/StatusService.java
@@ -0,0 +1,70 @@
+package com.github.tdotdm.timesheet.library;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StatusService {
+    private final LocationService locationService;
+    private final TimesheetService timesheetService;
+
+    public void status() {
+        final boolean workingDirectoryIsReady = isWorkingDirectoryReady();
+        if (workingDirectoryIsReady) {
+            log.info("Reading time sheet.");
+            timesheetService
+                    .read()
+                    .getLatestDay()
+                    .flatMap(Day::getLatestEntry)
+                    .ifPresent(latestEntry -> {
+                        final String timestamp = latestEntry.getTimestamp();
+                        final Action.Type type = latestEntry.getType();
+
+                        logStatus(timestamp, type);
+                    });
+        } else {
+            log.error("Working directory is not ready; application cannot continue.");
+        }
+    }
+
+    private boolean isWorkingDirectoryReady() {
+        final Optional<String> optionalTimesheetLocation = locationService.getTimesheetLocation();
+        if (!optionalTimesheetLocation.isPresent()) {
+            log.error("Cannot get location of time sheet.");
+
+            return false;
+        }
+
+        final String timesheetLocation = optionalTimesheetLocation.get();
+        log.info("Location of time sheet is '{}'.", timesheetLocation);
+        try {
+            log.info("Checking if there is already a time sheet.");
+            final File file = new File(timesheetLocation);
+            if (!file.exists()) {
+                log.error("Time sheet does not exist; creating a new one.");
+                return file.createNewFile();
+            }
+
+            return true;
+        } catch (final IOException e) {
+            log.error("Cannot create time sheet.");
+        }
+
+        return false;
+    }
+
+    private void logStatus(final String timestamp,
+                           final Action.Type type) {
+        log.info("********** TIMESHEET STATUS **********");
+        log.info("Timestamp: {}.", timestamp);
+        log.info("Action.Type: {}.", type);
+        log.info("**************************************");
+    }
+}
